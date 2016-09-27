fd900a1845f6ac69c7f380f50620138432b20e18
santonel <santonel@pinncorp.com>
2016-09-27 16:58:03 -0300

    Gulp with IF

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,30 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
+var util = require('gulp-util'); //allow console log functions.
+var gulpif = require('gulp-if'); // second argument is a stream not function
+var args = require('yargs').argv;
 
 gulp.task('vet', function() {
+	log('Analyzing source with JSHint and JSCS');
 	return gulp
 		.src(['./scr/**//.js', './*.js'])
-		.pipe(jscs())
+		.pipe(gulpif(args.verbose, jscs()))
 		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish', {verbose: true})); //it is needed for jshint
+		.pipe(jshint.reporter('jshint-stylish', {verbose: true})) //it is needed for jshint
+		.pipe(jshint.reporter('fail'));
 });
+
+function log(msg) {
+	if(typeof(msg) === 'object') {
+		for (var item in msg) {
+			if (msg.hasOwnProperty(item)) {
+				util.log(util.colors.blue(msg[item]));
+			}
+		}
+	}
+	else {
+		util.log(util.colors.blue(msg));
+	}
+}
+
